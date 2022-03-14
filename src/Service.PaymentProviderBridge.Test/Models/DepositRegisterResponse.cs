eb2311dcacc41c44caa6678ec26f9492e3dc9b56
dmitriy.b <dmitriy.b@smplt.net>
2022-03-14 11:54:40 +0500

    Add secure callback

---
+++ src/Service.PaymentProviderBridge.Test/Models/DepositRegisterResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace Service.PaymentProviderBridge.Test.Models
+{
+	public class DepositRegisterResponse
+	{
+		public string State { get; set; }
+
+		public string ExternalId { get; set; }
+
+		public string RedirectUrl { get; set; }
+	}
+}
--- src/Service.PaymentProviderBridge.Test/Services/PaymentProviderGrpcServiceTest.cs
+++ src/Service.PaymentProviderBridge.Test/Services/PaymentProviderGrpcServiceTest.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using Service.PaymentDeposit.Domain.Models;
 using Service.PaymentDepositRepository.Domain.Models;
+using Service.PaymentProviderBridge.Test.Models;
 using Service.PaymentProviderBridge.Test.Settings;
 
 namespace Service.PaymentProviderBridge.Test.Services
@@ -10,6 +13,9 @@ namespace Service.PaymentProviderBridge.Test.Services
 	public class PaymentProviderGrpcService : IPaymentProviderGrpcService
 	{
 		private readonly ILogger<PaymentProviderGrpcService> _logger;
+		private static readonly HttpClient Client;
+
+		static PaymentProviderGrpcService() => Client = new HttpClient();
 
 		public PaymentProviderGrpcService(ILogger<PaymentProviderGrpcService> logger) => _logger = logger;
 
@@ -23,14 +29,37 @@ namespace Service.PaymentProviderBridge.Test.Services
 				.Replace("#callback-url#", settings.CallbackUrl, StringComparison.OrdinalIgnoreCase)
 				.Replace("#info#", $"{request.Amount} {request.Currency}", StringComparison.OrdinalIgnoreCase);
 
-			_logger.LogDebug("Redirecting user to external pay system url: {url}", externalUrl);
+			DepositRegisterResponse registerResponse;
+			using (HttpResponseMessage response = Client.GetAsync(externalUrl).Result)
+				using (HttpContent content = response.Content)
+				registerResponse = JsonConvert.DeserializeObject<DepositRegisterResponse>(content.ReadAsStringAsync().Result);
+
+			if (registerResponse == null)
+			{
+				_logger.LogError("External id not recieved for url {url} with request {@request}!", externalUrl, request);
+
+				return ValueTask.FromResult(new ProviderDepositGrpcResponse
+				{
+					State = TransactionState.Error
+				});
+			}
+
+			_logger.LogDebug("Accepted response {@registerResponse} by url {url} for request {@request}!", registerResponse, externalUrl, request);
 
 			return ValueTask.FromResult(new ProviderDepositGrpcResponse
 			{
-				State = TransactionState.Accepted,
-				RedirectUrl = externalUrl
+				State = GetState(registerResponse.State),
+				ExternalId = registerResponse.ExternalId,
+				RedirectUrl = registerResponse.RedirectUrl
 			});
 		}
+
+		private static TransactionState GetState(string state) =>
+			state switch {
+				"accept" => TransactionState.Accepted,
+				"reject" => TransactionState.Rejected,
+				"approve" => TransactionState.Approved, 
+				_ => TransactionState.Error};
 
 		private static string SetTransactionId(string urlTemplate, Guid? id) => urlTemplate.Replace("#transaction-id#", id.ToString(), StringComparison.OrdinalIgnoreCase);
 	}
